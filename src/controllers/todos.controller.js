dd51b9309e7614280be5a26a7c409c96c630aa6f
Katia Jauregui <katiajauregui@gmail.com>
2023-01-10 22:18:42 -0600

    add getTodoById, createTodo, updateTodo, deleteTodo

--- src/controllers/todos.controller.js
+++ src/controllers/todos.controller.js
@@ -3,11 +3,32 @@ const todosServices = require('../services/todos.service');
 const getAllTodos = async (req, res) => {
     try {
         const result = await todosServices.getAll(); 
-        res.status(200).json(result);
+        // res.status(200).json(result);
+        res.json(result) // es lo mismo que el de arriba en este caso
     } catch (error) {
         res.status(400).json(error.message);
     }
 };
+
+const getTodoById = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const result = await todosServices.getById(id);
+        res.json(result);
+    } catch (error) {
+        res.status(400).json(error.message);
+    }
+};
+
+const createTodo = async (req, res) => {
+    try {
+        const todo = req.body;
+        const result = await todosServices.create(todo);
+        res.json(result)
+    } catch (error) {
+        res.status(400).json(error.message);
+    }
+}
 
 const updateTodo = async (req, res) => {
     try {
@@ -20,6 +41,20 @@ const updateTodo = async (req, res) => {
     }
 };
 
+const deleteTodo = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const result = await todosServices.delete(id);
+        res.json(result);
+    } catch (error) {
+        res.status(400).json(error.message);
+    }
+}
+
 module.exports = {
-    getAllTodos
+    getAllTodos,
+    updateTodo,
+    getTodoById,
+    createTodo,
+    deleteTodo
 }
--- src/services/todos.service.js
+++ src/services/todos.service.js
@@ -10,9 +10,35 @@ class todosServices {
         }
     }
 
+    static async getById(id){
+        try {
+            const result = await Todos.findByPk(id);
+            return result;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    static async create(todo) {
+        try {
+            const result = await Todos.create(todo);
+        } catch (error) {
+            throw error;
+        }
+    }
+
     static async update(id, field) {
         try {
             const result = await Todos.update(field, {where: {id}});
+            return result;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    static async delete(id) {
+        try {
+            const result = await Todos.destroy({ where: { id } });
             return result;
         } catch (error) {
             throw error;
